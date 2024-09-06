f2396902f02954130b4f7cce5edcf4ffaadb0bd7
tais defante <taisadefante@hotmail.com>
2024-09-06 10:07:33 -0300

    finalização projeto

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+import React from "react";
+
+export default (props) => (
+  <footer className="footer">
+    <span>
+      Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+      <strong>
+        {" "}
+        Tais<span className="text-danger">Defante</span> com o curso Cod3r.
+      </strong>
+    </span>
+  </footer>
+);
